aa7344404e7d4105394d1b71ffb38a89cf0b8f52
Joe Uttaro <joe@norex.ca>
2017-09-12 10:51:08 -0300

    addresses #2 - cleans up wp header

--- theme_files/functions.php
+++ theme_files/functions.php
@@ -79,3 +79,29 @@ function set_posts_per_search_page( $query ) {
 
     return $query;
 }
+
+add_action('wp_enqueue_scripts', 'clean_wordpress_header');
+function clean_wordpress_header(){
+    wp_deregister_script('comment-reply');
+
+    remove_action('admin_print_styles',     'print_emoji_styles');
+    remove_action('wp_head',                'print_emoji_detection_script', 7);
+    remove_action('admin_print_scripts',    'print_emoji_detection_script');
+    remove_action('wp_print_styles',        'print_emoji_styles');
+    remove_filter('wp_mail',                'wp_staticize_emoji_for_email');
+    remove_filter('the_content_feed',       'wp_staticize_emoji');
+    remove_filter('comment_text_rss',       'wp_staticize_emoji');
+    remove_action('wp_head',                'rest_output_link_wp_head');
+    remove_action('wp_head',                'wp_oembed_add_discovery_links');
+    remove_action('template_redirect',      'rest_output_link_header', 11, 0);
+    remove_action('wp_head',                'rsd_link');
+    remove_action('wp_head',                'wlwmanifest_link');
+    remove_action('wp_head',                'wp_shortlink_wp_head');
+    remove_action('wp_head',                'wp_generator');
+
+    add_filter('emoji_svg_url', '__return_false');
+    if(!is_front_page()){
+        wp_dequeue_style('sb_instagram_styles');
+        wp_dequeue_style('sb_instagram_icons');
+    }
+}
